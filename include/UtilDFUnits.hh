8b9c1658973872a119176218583f6c9ab9776c22
David Flay <flay@enpflay.jlab.org>
2021-05-20 09:04:41 -0500

    Added a units namespace

---
+++ include/UtilDFUnits.hh
@@ -0,0 +1,24 @@
+#ifndef UTIL_DF_UNITS_HH
+#define UTIL_DF_UNITS_HH
+
+// constants and enumerated types 
+
+namespace util_df {
+ 
+   namespace Units {
+
+     enum unitClass{
+	 kCGS,
+	 kMKS
+      };
+
+     enum bFieldUnits { 
+	kTesla,
+	kGauss
+     };
+
+   } //::units 
+
+} //::util_df
+
+#endif 
