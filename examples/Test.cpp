ace3634362bb20edd5cdfea0b01ac8135391cc35
David Flay <flay@enpflay.jlab.org>
2022-02-08 10:27:45 -0500

    Major update to the JSONManager to handle strings properly for conversion to decimal types.  Better example code.

--- examples/Test.cpp
+++ examples/Test.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 
+#include "JSONManager.hh"
 #include "CSVManager.hh"
 #include "UtilDFGraph.hh"
 #include "UtilDFMath.hh"
@@ -13,8 +14,8 @@
 int Test(){
 
    // luminosity for a LD2 target (per nucleon)  
-   A   = 2;           // number of nucleons (= Z + N)  
-   rho = 0.161246;    // density in g/cm^3  
+   double A   = 2;           // number of nucleons (= Z + N)  
+   double rho = 0.161246;    // density in g/cm^3  
    double I = 30E-6;  // 30 uA  
    double x = 15.0;   // 15 cm thick 
    double L = util_df::CrossSection::GetLuminosity(I,rho,x,A);
@@ -22,7 +23,7 @@ int Test(){
 
    // read data from a csv file and make a plot 
    util_df::CSVManager *myCSV = new util_df::CSVManager(); 
-   myCSV->ReadFile("data.csv",true); 
+   myCSV->ReadFile("./input/data.csv",true); 
 
    std::vector<std::string> header;
    myCSV->GetHeader(header); 
@@ -30,17 +31,40 @@ int Test(){
    std::string yAxisLabel = header[1]; 
 
    // get vectors of data   
-   std::vector<double> x,y;
-   myCSV->GetColumn_byName<double>(xAxisLabel,x); 
-   myCSV->GetColumn_byName<double>(yAxisLabel,y); 
+   std::vector<double> X,Y;
+   myCSV->GetColumn_byName<double>(xAxisLabel,X); 
+   myCSV->GetColumn_byName<double>(yAxisLabel,Y); 
  
    // do basic statistics  
-   double my = util_df::Math::GetMean<double>(y); 
-   double sy = util_df::Math::GetStandardDeviation<double>(y); 
+   double my = util_df::Math::GetMean<double>(Y); 
+   double sy = util_df::Math::GetStandardDeviation<double>(Y); 
    std::cout << Form("y stats: mean = %.3lf, stdev = %.3lf",my,sy) << std::endl; 
+
+   // read a JSON file
+   util_df::JSONManager *myJSON = new util_df::JSONManager();
+   myJSON->ReadFile("./input/data.json"); 
+   myJSON->Print(); 
+
+   std::string v1 = myJSON->GetValueFromKey_str("key-01"); 
+   double v2      = myJSON->GetValueFromKey<double>("key-02"); 
+   double v3      = myJSON->GetValueFromKey<double>("key-03"); 
+   double v4      = myJSON->GetValueFromSubKey<double>("key-04","subkey"); 
+
+   int NPTS = myJSON->GetValueFromKey<int>("npts");
+   std::vector<int> vv; 
+   myJSON->GetVectorFromKey<int>("key-05",NPTS,vv);
+   std::vector<int> vv2; 
+   myJSON->GetVectorFromKey<int>("key-06",NPTS,vv2);
+   
+   std::cout << Form("JSON data: key-01: %s, key-02: %.2lf, key-03: %.2lf, key-04: %.2lf",v1.c_str(),v2,v3,v4) << std::endl;
+
+   std::cout << "Vector from key-05: " << std::endl; 
+   for(int i=0;i<NPTS;i++) std::cout << vv[i] << std::endl;  
+   std::cout << "Vector from key-06: " << std::endl; 
+   for(int i=0;i<NPTS;i++) std::cout << vv2[i] << std::endl;   
  
    // create graphs    
-   TGraph *g = util_df::Graph::GetTGraph(x,y); 
+   TGraph *g = util_df::Graph::GetTGraph(X,Y); 
    util_df::Graph::SetParameters(g,20,kBlue);
 
    TCanvas *c1 = new TCanvas("c1","Data",500,500);
@@ -51,18 +75,20 @@ int Test(){
    g->Draw("ap");
    c1->Update();
 
+   const int N = X.size();
+
    // create and print to file a table of data
    util_df::CSVManager *myCSV2 = new util_df::CSVManager(); 
-   myCSV2->InitTable(2,2);      // set size of table 
+   myCSV2->InitTable(N,2);      // set size of table: N rows, 2 columns 
    myCSV2->SetHeader("x,y");    // set the header 
-   // set data by row (first index) and column (second index) 
-   myCSV2->SetElement<double>(0,0,5); 
-   myCSV2->SetElement<double>(0,1,10);
-   myCSV2->SetElement<double>(1,0,16);
-   myCSV2->SetElement<double>(1,1,27);
-   myCSV2->WriteFile("table.csv"); 
+   // set data by column using vector data 
+   myCSV2->SetColumn<double>(0,X); 
+   myCSV2->SetColumn<double>(1,Y); 
+   myCSV2->WriteFile("./output/table.csv"); 
 
    delete myCSV;
    delete myCSV2;
+   delete myJSON; 
+
    return 0;
 }
--- include/JSONManager.hh
+++ include/JSONManager.hh
@@ -5,7 +5,9 @@
 #include <iostream>
 #include <fstream>
 
-#include "json.hh" 
+#include "json.hh"
+
+#include "UtilDFConstants.hh" 
 
 using json = nlohmann::json; 
 
@@ -15,6 +17,23 @@ namespace util_df {
 
       private:
 	 json fObject;
+
+	 template <typename T>
+	    int CheckType(T data) const{
+	       int rc=0;
+	       if( std::is_arithmetic<T>::value ){
+		  if( std::is_integral<T>::value ){
+		     // it's an integer or boolean 
+		     rc = 0;
+		  }else{
+		     // it's a double or float 
+		     rc = 1;
+		  }
+	       }else{
+		  rc = 2; // it's a string
+	       }
+	       return rc;
+	    }
 
       public:
 	 JSONManager(const char *filepath="NONE");
@@ -32,32 +51,77 @@ namespace util_df {
 
 	 // templated methods 
 	 template <typename T> T GetValueFromKey(std::string key) const {
+	    // this function is to retrieve a number, but could be stored as a string in the JSON object 
+            std::string DATA="";
+            T val; 
+            int outDataType = CheckType<T>(val); 
             bool exist = DoesKeyExist(key);
 	    if(exist){ 
-	       return (T)fObject[key];
+	       // key exists 
+	       if( fObject[key].is_string() ){
+		  // data is a string type, convert to int or double 
+		  DATA = fObject[key].get<std::string>(); 
+		  if(outDataType==Constants::kInteger) val = std::atoi( DATA.c_str() ); 
+		  if(outDataType==Constants::kDouble)  val = std::atof( DATA.c_str() ); 
+	       }else{
+		  // data isn't a string, just typecast and store the value  
+		  val = (T)(fObject[key]);
+	       } 
 	    }else{
-	       return (T)(0);
-            } 
+	       // key doesn't exist, return 0
+	       val = (T)(0);
+            }
+	    return val; 
          }  
 
 	 template <typename T> T GetValueFromSubKey(std::string key,std::string subKey) const {
-	    bool exist = DoesKeyExist(key);
- 	    if(exist){ 
-	       return (T)fObject[key][subKey];
+	    // this function is to retrieve a number, but could be stored as a string in the JSON object 
+            std::string DATA="";
+            T val; 
+            int outDataType = CheckType<T>(val); 
+            bool exist = DoesKeyExist(key);
+	    if(exist){ 
+	       // key exists 
+	       if( fObject[key][subKey].is_string() ){
+		  // data is a string type, convert to int or double 
+		  DATA = fObject[key][subKey].get<std::string>(); 
+		  if(outDataType==Constants::kInteger) val = std::atoi( DATA.c_str() ); 
+		  if(outDataType==Constants::kDouble)  val = std::atof( DATA.c_str() ); 
+	       }else{
+		  // data isn't a string, just typecast and store the value  
+		  val = (T)(fObject[key][subKey]);
+	       } 
 	    }else{
-	       return (T)(0);
-            } 
+	       // key doesn't exist, return 0
+	       val = (T)(0);
+            }
+	    return val; 
          }
 
          template <typename T> 
 	    int GetVectorFromKey(std::string key,int N,std::vector<T> &data){
+               std::string DATA="";
                // first check if the key exists
 	       T arg;
+	       int outDataType = CheckType<T>(arg); 
 	       bool exist = DoesKeyExist(key);
 	       if(exist){
 		  // found the key, fill the vector
 		  for(int i=0;i<N;i++){
-		     arg = (T)fObject[key][i];
+                     // DATA = fObject[key][i];
+                     // arg = GetValueFromSubKey<T>(key,i);  
+		     // DATA = fObject[key][i].get<std::string>(); // fObject[key];  
+		     // if(dataType==Constants::kInteger) arg = std::atoi( DATA.c_str() ); 
+		     // if(dataType==Constants::kDouble)  arg = std::atof( DATA.c_str() );
+		     if( fObject[key][i].is_string() ){
+			// data is a string type, convert to int or double 
+			DATA = fObject[key][i].get<std::string>(); 
+			if(outDataType==Constants::kInteger) arg = std::atoi( DATA.c_str() ); 
+			if(outDataType==Constants::kDouble)  arg = std::atof( DATA.c_str() ); 
+		     }else{
+			// data isn't a string, just typecast and store the value  
+			arg = (T)(fObject[key][i]);
+		     } 
 		     data.push_back(arg);
 		  }
 	       }else{
